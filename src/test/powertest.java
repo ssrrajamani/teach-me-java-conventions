d14e9a0b80f36e7564368f500f98004c8546b77a
ssrrajamani <60108232+ssrrajamani@users.noreply.github.com>
2022-02-12 23:06:51 +0530

    Update powertest.java

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -5,22 +5,22 @@ import org.junit.Test;
 
 public class powertest {
     @Test
-    public void one_raised_to_one_is_1() {
-        assert powerfinder.OF(1,1) == 1;
+    public void one_raised_to_one_is_one() {
+        assert Power.powerFinder(1,1) == 1;
     }
 
     @Test
-    public void two_raised_to_one_is_2() {
-        assert powerfinder.OF(2,1) == 2;
+    public void two_raised_to_one_is_two() {
+        assert Power.powerFinder(2,1) == 2;
     }
 
     @Test
-    public void two_power_2_is_4() {
-        assert powerfinder.OF(2, 2) == 4;
+    public void two_raised_to_two_is_four() {
+        assert Power.powerFinder(2, 2) == 4;
     }
 
     @Test
-    public void power_of_2_and_3_is_9) {
-        assert powerfinder.OF(3, 2) == 9;
+    public void three_raised_to_three_is_nine) {
+        assert Power.powerFinder(3, 2) == 9;
     }
 }
